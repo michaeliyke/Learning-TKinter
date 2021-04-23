88adaa28127b682b852362709bc3938d9294901d
michaeliyke <ymichaelc@gmail.com>
2021-04-24 00:10:24 +0100

    Button Widget
    Simple button to terminate a counter

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,26 +1,37 @@
 from tkinter import Tk, Label, StringVar, RAISED, Button, LEFT
 
-root = Tk()
-root.title("First GUI application")
-txtVar = StringVar()
-x = Label(
-	root, 
-	textvariable=txtVar,
-	relief=RAISED,
-	fg="indigo" ,
-	bg="silver",
-	font=("jost", 30, "bold")
-	)
-txtVar.set("Welcome to TKinter Framework!")
-# x.grid()
-x.pack()
 
+def count1():
+	global counter
+	counter += 1
+	label.config(text=str(counter))
+	label.after(1000, count1)
+
+def count(label):
+	counter = 0
+	count1()
+
+
+root = Tk()
+root.title("Counting seconds")
+label = Label(
+	root, 
+	fg="blue",
+	font=("jost", 25)
+)
+label.pack()
+
+counter = 0
+
+count(label)
 btn = Button(
 	root, 
-	text="Exit", 
-	fg="crimson", 
-	command=quit,
+	text="stop", 
+	width=25, 
+	command=root.destroy, 
 	font=("jost", 14, "bold")
 	)
-btn.pack(side=LEFT)
+btn.pack()
+
+
 root.mainloop()
