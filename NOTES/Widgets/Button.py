be223d88fc655b1cdac7b37ebf6235b92f084b71
michaeliyke <ymichaelc@gmail.com>
2021-04-24 00:15:24 +0100

    NOTES
    About the Button widget

---
+++ NOTES/Widgets/Button.py
@@ -0,0 +1,26 @@
+# Button WIDGET NOTES
+
+"""
+Button is a standard TKinter widget used for varoius kinds of button
+functionalities. it is a widget designed for user to interact with.
+
+Button is used for:
+	Display text
+	Display image
+
+Parameteres:
+	1. name of parenting loop. This label will become its child
+	command: The function invoked when it's clicked
+	text. text to 
+	bg: Background color
+	fg: Foreground color i.e font color
+	textvariable: used to set or change the text 
+	font: For specifying font_family, font-size, and font-weight
+	relief: for controling border intensity - RAISED etc
+
+Methods:
+	.pack() - center label at all times
+	.grid() - align to center of grid - not clear yet
+
+technical information:
+	ALALAL
