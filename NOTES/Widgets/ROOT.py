9ac605dd287ca8512f136a2fe3916a935f580feb
michaeliyke <ymichaelc@gmail.com>
2021-04-23 22:58:23 +0100

    NOTES
    About root widget

---
+++ NOTES/Widgets/ROOT.py
@@ -0,0 +1,12 @@
+# NOTES ON ROOT WIDGET
+
+"""
+root widget is the tkinter window with the title bar
+
+root contains:
+	the created window
+	the title bar
+
+technical information:
+	the root widget must be created before other widgets
+	there can only be one root widget per program
