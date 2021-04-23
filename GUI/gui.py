8c47e8afc2c512d523fe22991d1c336bca957651
michaeliyke <ymichaelc@gmail.com>
2021-04-23 22:32:40 +0100

    Label Widget
    Simple text

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,6 +1,10 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 
 root = Tk()
 root.title("First GUI application")
+	
+x = Label(root, text="Hello World!", font=("jost", 30))
+# x.grid()
+x.pack()
 
 root.mainloop()
