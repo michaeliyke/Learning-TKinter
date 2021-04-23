fe778f7b94874b5b4e590dcdcd14d1b593258157
michaeliyke <ymichaelc@gmail.com>
2021-04-23 22:58:59 +0100

    NOTES
    About the Label widget

---
+++ NOTES/Widgets/Label.py
@@ -0,0 +1,24 @@
+# Label WIDGET NOTES
+
+"""
+LabeL is the easiest widget
+
+LabeL is used for:
+	Display text
+	Display image
+
+Parameteres:
+	1. name of parenting loop. This label will become its child
+	text. text to 
+	bg: Background color
+	fg: Foreground color i.e font color
+	textvariable: used to set or change the text 
+	font: For specifying font_family, font-size, and font-weight
+	relief: for controling border intensity - RAISED etc
+
+Methods:
+	.pack() - center label at all times
+	.grid() - align to center of grid - not clear yet
+
+technical information:
+	ALALAL
