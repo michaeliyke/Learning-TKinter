dc2486d2b0a2e9f96ca9c47422bd028897f94db6
michaeliyke <ymichaelc@gmail.com>
2021-04-24 22:37:30 +0100

    NOTES
    About the checkbuttons

---
+++ NOTES/Widgets/Checkbox.py
@@ -0,0 +1,24 @@
+# Checkbox WIDGET NOTES
+
+"""
+Checkbox permits user to select a number of different option
+s
+Checkbox is used for:
+	Display text
+	Display image
+
+Parameteres:
+	1. name of parenting loop. This Checkbox will become its child
+	text. text to 
+	bg: Background color
+	fg: Foreground color i.e font color
+	textvariable: used to set or change the text 
+	font: For specifying font_family, font-size, and font-weight
+	relief: for controling border intensity - RAISED etc
+
+Methods:
+	.pack() - center label at all times
+	.grid() - align to center of grid - not clear yet
+
+technical information:
+	ALALAL
