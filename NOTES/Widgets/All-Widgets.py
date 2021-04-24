2bc9c57e2bc3222a063091510f5325aa95c9d51f
michaeliyke <ymichaelc@gmail.com>
2021-04-24 08:59:53 +0100

    NOTES
    Common parameters, properties, and methods for all widgets

---
+++ NOTES/Widgets/All-Widgets.py
@@ -0,0 +1,29 @@
+# ALL WIDGETS NOTE
+
+"""
+These notes are about what they might all posibly have in common
+It's also about what some or many of them have in common
+
+"""
+
+# Common arguments
+"""
+bg='white'
+bd=---border things
+cursor: --things
+relief: --type of border---
+fg='green'
+activebackground='cyan'
+font=('jost', 12, 'bold')
+text='Like hell you love mangoes'
+width=25
+height=10
+
+
+"""
+
+# Methods
+"""
+.pack() & .grid()
+	root frame
+	All frames
