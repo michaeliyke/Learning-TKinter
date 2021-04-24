3eea7beb83c03f54d951006dd9262e16da9f3d09
michaeliyke <ymichaelc@gmail.com>
2021-04-24 08:57:58 +0100

    NOTES
    About the Frame widget

---
+++ NOTES/Widgets/Frame.py
@@ -0,0 +1,29 @@
+# Frame WIDGET NOTES
+
+"""
+Frame is used for grouping and organizing other widgets.
+It is mainly used as a container or a geometric master for other widgets while 
+   arranging their positions, layouts, and paddings
+Frame can also serve as a foundation for implementing complex widgets
+
+Frame is used for:
+	Grouping & organizing widgets
+	Provide positioning, layout & padding for widgets
+	Implementing cimplex widgets
+
+Parameteres:
+	1. name of parenting frame or root. This widget will become its child
+	activebackground: 
+	
+	bg: Background color
+	fg: Foreground color i.e font color
+	textvariable: used to set or change the text 
+	font: For specifying font_family, font-size, and font-weight
+	relief: for controling border intensity - RAISED etc
+
+Methods:
+	.pack() - center label at all times
+	.grid() - align to center of grid - not clear yet
+
+technical information:
+	ALALAL
