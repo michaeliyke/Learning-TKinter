2f4a8b6a0b0c8603ff3c4026ec3311b8f714d7f2
michaeliyke <ymichaelc@gmail.com>
2021-04-26 09:08:26 +0100

    Geometry .pack()
    Use .pack() to organize

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,13 +1,35 @@
-from tkinter import Tk, Label, StringVar, OptionMenu
+from tkinter import Tk, Label, X, Y, LEFT
 
 
 app = Tk()
 app.title("Window")
 app.geometry("600x500")
-variable = StringVar(app)
-variable.set("May be")
+font=("roboto", 16)
 
-options = OptionMenu(app, variable, "Yes", "No", "Not sure", "May be")
-options.pack()
+Label(
+	app, 
+	text="Favourite Languages", 
+	bg="#abcabc",
+	fg="#fff",
+	font=("impact", 20)
+	).pack(fill=X)
+i = 0
+for lang in ("C", "C++", "Java", "Python", "JavaScript", "PHP"):
+	Label(
+		app, 
+		text=lang , 
+		bg="#000", 
+		fg="#ebebeb", 
+		font=font
+		).pack(fill=X, pady=5, padx=12)
+
+for lang in ("Ruby", "Perl", "Cobol", "Lisp", "Assembly"):
+	Label(
+		app, 
+		text=lang,
+		bg="#777",
+		fg="orange",
+		font=("cursive", 12)
+		).pack(padx=4, side=LEFT)
 
 app.mainloop()
